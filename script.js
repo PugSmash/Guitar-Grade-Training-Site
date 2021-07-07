1c5be37d438b50178a711460ae3a150c7517ffe2
PugSmash <63645342+PugSmash@users.noreply.github.com>
2021-07-07 11:01:56 +0100

    On branch master  Initial commit
     Changes to be committed:
    	new file:   index.html
    	new file:   script.js
    	new file:   style.css

---
+++ script.js
@@ -0,0 +1,22 @@
+var base_chord = ["A", "B", "C", "D", "E", "F", "G"];
+var maj_min = ["maj", "m", " "];
+var seven = ["7", " "];
+var flats_and_sharps = ["♭", "#", " "];
+
+function generator() {
+  
+  
+  var random_num_7 = Math.floor(Math.random() * 1);
+  var random_number_base = Math.floor(Math.random() * 6);
+  var random_number_maj = Math.floor(Math.random() * 3)
+  var random_num_flats = Math.floor(Math.random() * 3)
+  
+  
+  console.log(random_number_maj);
+  
+ 
+  document.getElementById("yes").innerHTML = base_chord[random_number_base] + flats_and_sharps[random_num_flats] + maj_min[random_number_maj] + seven[random_num_7];
+  
+ 
+ 
+}
